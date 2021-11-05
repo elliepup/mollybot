700d66c00e989fca7b7ea44213e7c4895a5d0e52
elliepup <49000519+elliepup@users.noreply.github.com>
2021-11-05 17:54:26 -0400

    Prefix embed

---
+++ commands/music/seek.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'seek',
+	description: 'Seek to a certain part of the song',
+    args: true,
+    usage: 'volume [seconds or MM:SS]',
+	execute(message, args) {
+            //have fun future self :)
+	},
+};
